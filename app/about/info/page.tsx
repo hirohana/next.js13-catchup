02126356b8d50baa7593197fa5390f0936133bbb
hirohana <hirohanahaba@gmail.com>
2023-06-08 20:11:30 +0900

    routing

---
+++ app/about/info/page.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>info</div>;
+};
+
+export default About;
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,20 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  team,
+  analytics,
 }: {
   children: React.ReactNode;
+  team: React.ReactNode;
+  analytics: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>{children}</div>
+        <div>{team}</div>
+        <div>{analytics}</div>
+      </body>
     </html>
   );
 }
